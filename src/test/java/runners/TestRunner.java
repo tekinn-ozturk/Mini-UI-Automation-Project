1afd7c873d49e1bd6e73f0ae3263d89692d04342
tekinn-ozturk <tekinoztrk10@gmail.com>
2026-09-20 11:36:02 +0300

    runner fixed

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,7 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "stepdefinitions"
+        glue = "stepdefinitions",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html"
+        }
 )
 public class TestRunner {
 
